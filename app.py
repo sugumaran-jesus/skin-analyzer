06b4ed77682e20f0282b4565a27954cb9630a162
sugumaran-jesus <suguks33317@gmail.com>
2026-04-15 18:53:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, render_template
-#import tensorflow as tf
+import tensorflow as tf
 import numpy as np
 from PIL import Image
+import os
+import gdown
 
 app = Flask(__name__)
+MODEL_PATH = "model/skin_model.h5"
+MODEL_URL = "https://drive.google.com/uc?id=1bqwEkTMcJODBNfsUpAsdDDM3PFYaszYY"
+
+# Download model if not exists
+if not os.path.exists(MODEL_PATH):
+    os.makedirs("model", exist_ok=True)
+    gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
 
 # Load model
-#model = tf.keras.models.load_model("model/skin_model.h5")
+model = tf.keras.models.load_model(MODEL_PATH)
 
 classes = ["acne","dry","normal","oily"]
 
@@ -15,44 +24,29 @@ def index():
     if request.method == "POST":
         file = request.files["image"]
 
-        img = Image.open(file).resize((128,128))
+        img = Image.open(file).convert("RGB").resize((128,128))
         img = np.array(img)/255.0
         img = img.reshape(1,128,128,3)
 
         prediction = model.predict(img)
         result = classes[np.argmax(prediction)]
 
-        # Cosmetic suggestion
         if result == "dry":
-         suggestion = (
-        "Your skin is dry. It lacks moisture and may feel rough or flaky. "
-        "Use a good moisturizer twice daily to keep your skin hydrated. "
-        "Drink plenty of water and avoid using harsh soaps or hot water."
-    )
+            suggestion = "Your skin is dry. Use moisturizer and drink water."
 
         elif result == "oily":
-         suggestion = (
-        "Your skin is oily. It produces excess sebum which can make your face look shiny. "
-        "Use an oil-free face wash twice daily to control oil. "
-        "Avoid heavy creams and prefer lightweight, non-comedogenic products."
-    )
+            suggestion = "Your skin is oily. Use oil-free cleanser."
 
         elif result == "acne":
-         suggestion = (
-        "Acne is detected on your skin. This may be caused by clogged pores or excess oil. "
-        "Use a gentle cleanser and avoid touching your face frequently. "
-        "Consider using products with salicylic acid or benzoyl peroxide."
-    )
+            suggestion = "You have acne. Use gentle cleanser and avoid touching face."
 
         else:
-         suggestion = (
-        "Your skin is normal. It is well-balanced and healthy. "
-        "Maintain a simple skincare routine with regular cleansing and moisturizing. "
-        "Protect your skin from sun exposure by using sunscreen daily."
-    )
+            suggestion = "Your skin is normal. Maintain simple skincare routine."
+
         return f"Skin: {result} | Suggestion: {suggestion}"
 
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
