c9920e31dd1f1734733a0852daf18104dc964080
sugumaran-jesus <suguks33317@gmail.com>
2026-04-15 19:53:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -22,31 +22,44 @@ classes = ["acne","dry","normal","oily"]
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    if request.method == "POST":
-        file = request.files["image"]
+    try:
+        if request.method == "POST":
 
-        img = Image.open(file).convert("RGB").resize((128,128))
-        img = np.array(img)/255.0
-        img = img.reshape(1,128,128,3)
+            if "image" not in request.files:
+                return "No image uploaded"
 
-        prediction = model.predict(img)
-        result = classes[np.argmax(prediction)]
+            file = request.files["image"]
 
-        if result == "dry":
-            suggestion = "Your skin is dry. Use moisturizer and drink water."
+            if file.filename == "":
+                return "No file selected"
 
-        elif result == "oily":
-            suggestion = "Your skin is oily. Use oil-free cleanser."
+            img = Image.open(file).convert("RGB").resize((128,128))
+            img = np.array(img)/255.0
+            img = img.reshape(1,128,128,3)
 
-        elif result == "acne":
-            suggestion = "You have acne. Use gentle cleanser and avoid touching face."
+            prediction = model.predict(img)
+            result = classes[np.argmax(prediction)]
 
-        else:
-            suggestion = "Your skin is normal. Maintain simple skincare routine."
+            if result == "dry":
+               suggestion = "Your skin is dry. Use moisturizer and drink water."
+
+            elif result == "oily":
+                 suggestion = "Your skin is oily. Use oil-free cleanser."
+
+            elif result == "acne":
+                 suggestion = "You have acne. Use gentle cleanser and avoid touching face."
+
+            else:
+                 suggestion = "Your skin is normal. Maintain simple skincare routine."
 
         return f"Skin: {result} | Suggestion: {suggestion}"
 
-    return render_template("index.html")
+        return render_template("index.html")
+
+    except Exception as e:
+        print("ERROR OCCURRED:")
+        print(traceback.format_exc())
+        return f"Server Error: {str(e)}"
 
 
 if __name__ == "__main__":
